4ccd269f6fbfbd73eff04f0b20d9d2de6b04246f
zhangqinghong <zhangqinghong@zhisland.com>
2022-03-24 19:22:20 +0800

    crypto file

--- src/decrypt.rs
+++ src/decrypt.rs
@@ -1,235 +1,119 @@
-use crypto::aes::{cbc_decryptor, cbc_encryptor, KeySize};
-use crypto::blockmodes::PkcsPadding;
-use crypto::buffer::{ReadBuffer, RefReadBuffer, RefWriteBuffer, WriteBuffer};
+use aesstream::{AesReader, AesWriter};
+use crypto::aessafe::{AesSafe128Decryptor, AesSafe128Encryptor};
 
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::Path;
+use std::io::{Cursor, Read};
+// use std::path::Path;
+use std::fs::remove_file;
+use std::string::ParseError;
 
-fn encrypt_aes(data: &[u8], key: &str, iv: &str) -> Vec<u8> {
-    let key1 = key.as_bytes();
-    let iv1 = iv.as_bytes();
-    let mut encryptor = cbc_encryptor(KeySize::KeySize128, &key1, &iv1, PkcsPadding);
-
-    let mut final_result = Vec::<u8>::new();
-    let mut read_buffer = RefReadBuffer::new(data);
-
-    let mut buffer = [0; 4096];
-    let mut write_buffer = RefWriteBuffer::new(&mut buffer);
-
-    let _result = encryptor.encrypt(&mut read_buffer, &mut write_buffer, true);
-
-    final_result.extend(
-        write_buffer
-            .take_read_buffer()
-            .take_remaining()
-            .iter()
-            .map(|&i| i),
-    );
-
-    final_result
-}
-
-fn decrypt_aes(data: &[u8], key: &str, iv: &str) -> Vec<u8> {
-    let key1 = key.as_bytes();
-    let iv1 = iv.as_bytes();
-    let mut decryptor = cbc_decryptor(KeySize::KeySize128, &key1, &iv1, PkcsPadding);
-
-    // let buffer1 = <[u8; 16]>::from_hex(msg);
-    // let buffer1 = match hex::decode(data) {
-    //     Ok(val) => val,
-    //     _ => Vec::<u8>::new(),
-    // };
-
-    let mut final_result = Vec::<u8>::new();
-    let mut read_buffer = RefReadBuffer::new(data);
-    let mut buffer = [0; 4096];
-    let mut write_buffer = RefWriteBuffer::new(&mut buffer);
-
-    // loop {
-    let _result = decryptor.decrypt(&mut read_buffer, &mut write_buffer, true);
-    final_result.extend(
-        write_buffer
-            .take_read_buffer()
-            .take_remaining()
-            .iter()
-            // .filter(|&i| *i != 0)
-            .map(|&i| i),
-    );
-    // String::from_utf8(final_result.clone()).unwrap()
-    final_result
-}
-
-pub fn encrypt_aes_file(file_path: &str, key: &str, iv: &str) {
-    let _key1 = key.as_bytes();
-    let _iv1 = iv.as_bytes();
-
-    let path = Path::new(file_path);
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
-        // 处理打开文件可能潜在的错误
-        Err(_why) => panic!("couldn't open {}", display),
-        Ok(file) => file,
-    };
-
-    let output_path = Path::new("/Users/zhangqinghong/study/rust/hls_parse/tests/test1.png");
-    let display1 = output_path.display();
-    let mut output_file = match File::create(&output_path) {
-        // 处理打开文件可能潜在的错误
-        Err(_why) => panic!("couldn't open {}", display1),
-        Ok(file) => file,
-    };
-
-    let mut count = 0;
-    loop {
-        let mut buf = vec![0; 16];
-        let n = file.read(&mut buf[..]).unwrap();
-        // buf = buf.iter().filter(|&i| *i != 0).map(|&i| i).collect();
-
-        if n == 0 {
-            break;
-        }
-        // if count == 4981 {
-        //     break;
-        // }
-
-        let res = encrypt_aes(&buf, key, iv);
-        println!("=={}=={}=={:?}", count, n, buf);
-        // println!(
-        //     "=={}==: {:?} : {}",
-        //     buf.len(),
-        //     String::from_utf8(buf.clone()),
-        //     hex::encode(&res)
-        // );
-
-        output_file.write_all(&res).expect("write failed");
-
-        // if
-        count += 1;
+#[allow(dead_code)]
+fn encrypt_aes(data: &[u8], key: &str) -> Result<Vec<u8>, ParseError> {
+    let encryptor = AesSafe128Encryptor::new(key.as_bytes());
+    let mut encrypted = Vec::new();
+    {
+        let mut writer = AesWriter::new(&mut encrypted, encryptor).unwrap();
+        writer.write_all(data);
     }
+    Ok(encrypted)
+}
+#[allow(dead_code)]
+fn decrypt_aes(data: &[u8], key: &str) -> Result<Vec<u8>, ParseError> {
+    let decryptor = AesSafe128Decryptor::new(key.as_bytes());
+    let mut reader = AesReader::new(Cursor::new(data), decryptor).unwrap();
+    let mut v = Vec::new();
+    reader.read_to_end(&mut v).unwrap();
+    Ok(v)
 }
 
-pub fn decrypt_aes_file(file_path: &str, key: &str, iv: &str) {
-    let _key1 = key.as_bytes();
-    let _iv1 = iv.as_bytes();
-
-    let path = Path::new(file_path);
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
+// pub fn encrypt_aes_file(src_file_path: &str, dist_file_path: &str, key: &str) {}
+#[allow(dead_code)]
+pub fn decrypt_aes_file(src_file_path: &str, dist_file_path: &str, key: &str) {
+    let file = match File::open(&src_file_path) {
         // 处理打开文件可能潜在的错误
-        Err(_why) => panic!("couldn't open {}", display),
+        Err(_why) => panic!("couldn't open"),
         Ok(file) => file,
     };
 
-    let output_path = Path::new("/Users/zhangqinghong/study/rust/hls_parse/tests/test2.png");
-    let display1 = output_path.display();
-    let mut output_file = match File::create(&output_path) {
+    let decryptor = AesSafe128Decryptor::new(key.as_bytes());
+    let mut reader = AesReader::new(file, decryptor).unwrap();
+    let mut v = Vec::new();
+    reader.read_to_end(&mut v).unwrap();
+
+    let mut output_file = match File::create(dist_file_path) {
         // 处理打开文件可能潜在的错误
-        Err(_why) => panic!("couldn't open {}", display1),
+        Err(_why) => panic!("couldn't open"),
         Ok(file) => file,
     };
-
-    let mut count = 0;
-    loop {
-        let mut buf = vec![0; 32];
-        let n = file.read(&mut buf[..]).unwrap();
-        // buf = buf.iter().filter(|&i| *i != 0).map(|&i| i).collect();
-
-        if n == 0 {
-            break;
-        }
-
-        // if count == 4981 {
-        //     break;
-        // }
-        let res = decrypt_aes(&buf, key, iv);
-        println!("=={}=={}=={:?}", count, n, res);
-        output_file.write_all(&res).expect("write failed");
-        // println!(
-        //     "=={}==: {:?} : {}",
-        //     buf.len(),
-        //     String::from_utf8(buf.clone()),
-        //     hex::encode(&res)
-        // );
-        count += 1;
-    }
+    output_file.write_all(&v).expect("write failed");
 }
-
-pub fn encrypt_aes_str(msg: &str, key: &str, iv: &str) -> String {
-    let res = encrypt_aes(msg.as_bytes(), key, iv);
-    hex::encode(&res)
+#[allow(dead_code)]
+pub fn encrypt_aes_str(msg: &str, key: &str) -> Result<String, ParseError> {
+    let res = encrypt_aes(msg.as_bytes(), key).unwrap();
+    Ok(hex::encode(&res))
+    // Ok("".to_string())
 }
-
-pub fn decrypt_aes_str(msg: &str, key: &str, iv: &str) -> String {
-    // let key1 = key.as_bytes();
-    // let iv1 = iv.as_bytes();
-    // let mut decryptor = cbc_decryptor(KeySize::KeySize128, &key1, &iv1, PkcsPadding);
-
-    // let buffer1 = <[u8; 16]>::from_hex(msg);
+#[allow(dead_code)]
+pub fn decrypt_aes_str(msg: &str, key: &str) -> Result<String, ParseError> {
     let buffer1 = match hex::decode(msg) {
         Ok(val) => val,
         _ => Vec::<u8>::new(),
     };
-    let res = decrypt_aes(&buffer1, key, iv);
-    String::from_utf8(res.clone()).unwrap()
 
-    // let mut final_result = Vec::<u8>::new();
-    // let mut read_buffer = RefReadBuffer::new(&buffer1);
-    // let mut buffer = [0; 4096];
-    // let mut write_buffer = RefWriteBuffer::new(&mut buffer);
-
-    // // loop {
-    // let _result = decryptor.decrypt(&mut read_buffer, &mut write_buffer, true);
-    // final_result.extend(
-    //     write_buffer
-    //         .take_read_buffer()
-    //         .take_remaining()
-    //         .iter()
-    //         .filter(|&i| *i != 0)
-    //         .map(|&i| i),
-    // );
-    // String::from_utf8(final_result.clone()).unwrap()
+    let res = decrypt_aes(&buffer1, key).unwrap();
+    Ok(String::from_utf8(res.clone()).unwrap())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::path::PathBuf;
 
     const CRYPTO_KEY: &str = "1000000000000000";
-    const CRYPTO_IV: &str = "0000000000000000";
 
     #[test]
     fn test_encrypt_aes() {
-        // assert_eq!(
-        //     "ab6c513b8f4e04d86a932b4552a09a46",
-        //     encrypt_aes_str(&"你好", &CRYPTO_KEY, &CRYPTO_IV)
-        // );
-
-        assert_eq!(
-            "e6996765a06345cf5860efa14ea8d36e",
-            encrypt_aes_str(&"#EXTM3U#", &CRYPTO_KEY, &CRYPTO_IV)
-        );
+        let en = encrypt_aes_str(&"1", &CRYPTO_KEY).unwrap();
+        assert_eq!("1", decrypt_aes_str(&en, &CRYPTO_KEY).unwrap());
     }
 
     #[test]
     fn test_decrypt_aes() {
         assert_eq!(
-            "你好",
-            decrypt_aes_str(&"ab6c513b8f4e04d86a932b4552a09a46", &CRYPTO_KEY, &CRYPTO_IV)
+            "1",
+            decrypt_aes_str(
+                &"0b95b23b8fe7f3a50722bfe5a841f3bef2ea97630e0fa6b8421437a33e8baa42",
+                &CRYPTO_KEY
+            )
+            .unwrap()
         );
     }
 
-    #[test]
-    fn test_encrypt_aes_file() {
-        let file_path = "/Users/zhangqinghong/Movies/1645153272333099.mp4";
-        encrypt_aes_file(&file_path, &CRYPTO_KEY, &CRYPTO_IV);
-    }
+    // #[test]
+    // fn test_encrypt_aes_file() {
+    //     let file_path = "/Users/zhangqinghong/Movies/1645153272333099.mp4";
+    //     encrypt_aes_file(&file_path, &CRYPTO_KEY, &CRYPTO_IV);
+    // }
 
     #[test]
     fn test_decrypt_aes_file() {
-        let file_path = "/Users/zhangqinghong/study/rust/hls_parse/tests/test1.png";
-        decrypt_aes_file(&file_path, &CRYPTO_KEY, &CRYPTO_IV);
+        let key = "44a7e2d6a8fdbaae";
+
+        let root_dir = std::env::current_dir().unwrap();
+
+        let mut src_path = root_dir.clone();
+        src_path.push(PathBuf::from("tests/data/1.ts"));
+
+        let mut dist_path = root_dir.clone();
+        dist_path.push(PathBuf::from("tests/data/1_1.ts"));
+
+        decrypt_aes_file(
+            src_path.to_str().unwrap(),
+            dist_path.to_str().unwrap(),
+            &key,
+        );
+
+        remove_file(dist_path);
     }
 }
